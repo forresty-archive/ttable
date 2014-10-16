9b57a84f582c47c848888b68617771f00e860b31
Forrest Ye <afu@forresty.com>
2014-10-16 14:58:25 +0800

    ※ ◎

--- spec/terminal/table_spec.rb
+++ spec/terminal/table_spec.rb
@@ -98,5 +98,15 @@ describe String do
       subject { '☻' }
       its(:twidth) { should == 1 }
     end
+
+    context '※' do
+      subject { '※' }
+      its(:twidth) { should == 1 }
+    end
+
+    context '◎' do
+      subject { '◎' }
+      its(:twidth) { should == 1 }
+    end
   end
 end
