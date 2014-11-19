404b99bb2a818445cb4a045f6bf95dc24e9855bc
Forrest Ye <afu@forresty.com>
2014-11-19 19:48:09 +0800

    ¥

--- spec/terminal/table_spec.rb
+++ spec/terminal/table_spec.rb
@@ -438,5 +438,10 @@ describe String do
       subject { '←' }
       its(:twidth) { should == 1 }
     end
+
+    context '¥' do
+      subject { '¥' }
+      its(:twidth) { should == 1 }
+    end
   end
 end
