23a1a975a06c2909c224c901eb3517e43d364e9c
Forrest Ye <afu@forresty.com>
2014-12-25 20:20:33 +0800

    handle ∩

--- spec/terminal/table_spec.rb
+++ spec/terminal/table_spec.rb
@@ -853,5 +853,10 @@ describe String do
       subject { '❀' }
       its(:twidth) { should == 1 }
     end
+
+    context '∩' do
+      subject { '∩' }
+      its(:twidth) { should == 1 }
+    end
   end
 end
