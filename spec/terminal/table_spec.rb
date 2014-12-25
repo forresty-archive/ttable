c718bfd9a9826458417e6854bc355d3481a70c18
Forrest Ye <afu@forresty.com>
2014-12-25 20:40:10 +0800

    more cases

--- spec/terminal/table_spec.rb
+++ spec/terminal/table_spec.rb
@@ -928,5 +928,40 @@ describe String do
       subject { '♻️' }
       its(:twidth) { should == 1 }
     end
+
+    context '♒️' do
+      subject { '♒️' }
+      its(:twidth) { should == 1 }
+    end
+
+    context '±' do
+      subject { '±' }
+      its(:twidth) { should == 1 }
+    end
+
+    context '✏' do
+      subject { '✏' }
+      its(:twidth) { should == 1 }
+    end
+
+    context '∪' do
+      subject { '∪' }
+      its(:twidth) { should == 1 }
+    end
+
+    context '♬' do
+      subject { '♬' }
+      its(:twidth) { should == 1 }
+    end
+
+    context '☜☞' do
+      subject { '☜☞' }
+      its(:twidth) { should == 2 }
+    end
+
+    context '' do
+      subject { '' }
+      its(:twidth) { should == 1 }
+    end
   end
 end
