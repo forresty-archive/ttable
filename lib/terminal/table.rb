704e1737391aa0454a29fba305b31b3402256c04
Forrest Ye <afu@forresty.com>
2015-02-26 17:22:09 +0800

    Cyrillic

--- lib/terminal/table.rb
+++ lib/terminal/table.rb
@@ -3,8 +3,7 @@
 require "gemoji"
 
 class String
-  CHAR_CODES_OF_WIDTH_1 = [1044, 1053, 1072, 1076,
-                           1079, 1090, 1096, 1342, 1608, 1641, 1782, 2919, 2920, 3232,
+  CHAR_CODES_OF_WIDTH_1 = [1342, 1608, 1641, 1782, 2919, 2920, 3232,
                            3237, 3248, 3267, 3589, 3665, 3844, 3900, 3901, 5026, 5046, 5072, 5603,
                            5608, 7447, 7461, 7500, 7506, 8198, 8203, 8212, 8214, 8216, 8217,
                            8220, 8221, 8226, 8230, 8242, 8248, 8251, 8254, 8316, 8317,
@@ -70,6 +69,8 @@ class String
       when (0x2B0..0x2FF) # Spacing Modifier Letters
         result += 1
       when (0x370..0x3FF) # Greek and Coptic
+        result += 1
+      when (0x400..0x482) # Cyrillic
         result += 1
 
       # http://www.unicode.org/charts/PDF/U20D0.pdf
