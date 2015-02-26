7cfa97de9ef3d43e6bebea554f82247c7cb5bd13
Forrest Ye <afu@forresty.com>
2015-02-26 16:47:58 +0800

    refactoring

--- lib/terminal/table.rb
+++ lib/terminal/table.rb
@@ -3,9 +3,7 @@
 require "gemoji"
 
 class String
-  CHAR_CODES_OF_WIDTH_0 = [768, 769, 776, 780, 785, 800, 801, 802, 804, 805, 807, 808,
-                           809, 811, 820, 821, 822, 823, 840, 847, 860, 862, 863, 865,
-                           1552, 1553, 1554, 1555, 1556, 1557, 1558, 1560, 1561, 1756,
+  CHAR_CODES_OF_WIDTH_0 = [1552, 1553, 1554, 1555, 1556, 1557, 1558, 1560, 1561, 1756,
                            2370, 2760, 3267, 3636, 3659, 7620, 7621, 8408, 8409, 8411]
 
   CHAR_CODES_OF_WIDTH_1 = [706, 707, 713, 714, 715, 717, 726, 728, 730, 757, 758, 920,
@@ -69,6 +67,8 @@ class String
 
       when (0x250..0x2AF) # IPA Extensions
         result += 1
+      when (0x300..0x36F) # Combining Diacritical Marks
+        result += 0
 
       # http://www.unicode.org/charts/PDF/UFF00.pdf
       # Halfwidth and Fullwidth Forms
