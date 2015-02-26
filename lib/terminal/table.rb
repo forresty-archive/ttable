e721d7ef86f5a96328a2fecd1642cee643ab6997
Forrest Ye <afu@forresty.com>
2015-02-26 17:58:06 +0800

    Number Forms

--- lib/terminal/table.rb
+++ lib/terminal/table.rb
@@ -5,7 +5,7 @@ require "gemoji"
 class String
   CHAR_CODES_OF_WIDTH_1 = [1608, 1641, 1782, 3232,
                            3237, 3248, 3267, 3589, 3665, 3844, 5026, 5046, 5072, 5603,
-                           5608, 7447, 7461, 7500, 7506, 8198, 8203, 8545, 9000, 9166,
+                           5608, 7447, 7461, 7500, 7506, 8198, 8203, 9000, 9166,
                            11015, 57643, 58141, 58370, 58381,
                            58387]
 
@@ -69,6 +69,8 @@ class String
       when (0x2070..0x209F) # Superscripts and Subscripts
         result += 1
       when (0x2100..0x214F) # Letterlike Symbols
+        result += 1
+      when (0x2150..0x218F) # Number Forms
         result += 1
       when (0x2190..0x21FF) # Arrows
         result += 1
